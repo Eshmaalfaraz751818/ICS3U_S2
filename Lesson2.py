c4960cf7562ac01390c4366d55a1a32f5547d813
Eshmaalfaraz751818 <751818@pdsb.net>
2025-02-04 21:13:56 -0500

    Create Lesson2.py

---
+++ Lesson2.py
@@ -0,0 +1,34 @@
+print(24)
+#24 is printed 
+amount=24
+print(amount)
+#amount is printed which is set to 24
+amount=24
+print("Two dozen is: ", amount)
+#amount 24 is printed as two dozen is 24
+amount=28
+print("twenty-eight is: ", amount)
+#amout is 28 its printed as twenty eight is 28
+amount = 8
+amount = amount + 1
+print("The amount is: ", amount)
+# The amouNt is equal to 8 and when the code says amount plus 1 it is 9
+amount=8
+amount += 1
+print("The amount is: ", amount)
+# The same code is printed with the more compact syntax
+amount = 8
+amount = amount * 2
+print(amount)
+# Amount is equal to eight and when we add * sign its multiplied by 2
+amount = 8
+amount *= 2
+print(amount)
+#The sane code is printed with more compacact systax 
+q = 18
+q = q - 1 # using subtraction
+q -= 1 # does the same thing
+print("The value is now: ", q) # prints out "The value is now: 16"
+q = q / 2 # using division
+q /= 2 # same thing
+print("The value is now: ", q) # prints out "The value is now: 4.0"
