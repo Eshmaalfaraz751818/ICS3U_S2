9d84d1de6a2c1d8df46eeddfdadc8e9c46131f27
Eshmaalfaraz751818 <751818@pdsb.net>
2025-02-06 12:59:16 -0500

    Create Lesson4.py

---
+++ Lesson4.py
@@ -0,0 +1,12 @@
+num = input("Please input a whole number: ") 
+num = int(num) 
+num2 = num / 6
+print("When", num, " is divided by 6", "the result is:", num2)
+#The code stopped because I needed to imput a whole number, once I imputed it, the code stated that num2 is 4. I expected the answer to be 4 but it printed as 4.0
+num = input("Please input a whole number: ") 
+num = int(num)
+num2= input("use any number expect 0:")
+num2= int(num2)
+num3 = num / num2
+print("When", num, " is divided by",num2, "the result is:", num3)
+# the code printed 4 as the result of deviding num 1 by num2. I put that num2 could be any number other than 0 because the asnwer would be undefined.
