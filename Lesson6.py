0d47e2fb14170beef779b9db1af355f53c7e440b
Eshmaalfaraz751818 <751818@pdsb.net>
2025-02-07 16:32:58 -0500

    Create Lesson6.py

---
+++ Lesson6.py
@@ -0,0 +1,15 @@
+import math
+a= input("please input your age:")
+b= input("please input your birth year:")
+# I coded to ask for the users age and birth year before continuing to the next step.
+import math
+a= input("please input your age:")
+a=float(a)
+b=input("please input your birth year:")
+b=float(b)
+c= (b*2)+5
+c= (c*50)+a
+c=(c-250)/100
+print("result is:", c)
+# The code took your birth year multiplied it by 2 then added 5. It took the result multiplied by 50 then added your age. It took the result subtracted 250 then devided by 100.
+# At the end the answer is yourbirth year with decimals of your age. 
