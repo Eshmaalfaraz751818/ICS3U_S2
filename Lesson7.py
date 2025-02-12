b2d620a3b797746a79d34c89f4ce357fe250ae37
Eshmaalfaraz751818 <751818@pdsb.net>
2025-02-12 12:47:48 -0500

    Update Lesson7.py

--- Lesson7.py
+++ Lesson7.py
@@ -4,15 +4,13 @@ x = input("Please input a whole number: ")
 x = int(x)
 y = input("Please input another nonzero whole number")
 y = int(y)
-print("Now deciding if", y, "is a factor of", x, "...")
-rem=0
+print("Now deciding if", y, "is a factar of", x, "...")
 if y!=0:
-  print("Yes!", y, "is  a factor of",x)
+  print("Yes!", y, "is a factar of", x)
   rem = x % y
   if rem == 0:
-    print("Yes! remiander is 0")
+    print("Yes! remainder is 0" )
   else:
-    print("No! The remainder is", rem)
-  
+    print("The remainder is not 0")
 else:
-     print("No!", y, "is not a foctor of", x)  
+    print("No!", y, "is not a foctar of", x)
